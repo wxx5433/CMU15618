0537cb3228507b3ac3cfc02e404b0128322a19e5
wxx5433 <wxx5433@gmail.com>
2015-03-01 13:47:07 -0500

    finish asst3

--- hw/asst3/part1/src/parallelSort.cpp
+++ hw/asst3/part1/src/parallelSort.cpp
@@ -6,10 +6,14 @@
 #include <cassert>
 #include <cmath>
 #include <mpi.h>
+#include <time.h>
 
 #include "parallelSort.h"
+#include "CycleTimer.h"
 
 using namespace std;
+
+#define NO_DEBUG
 
 void printArr(const char* arrName, int *arr, size_t size, int procId) {
 #ifndef NO_DEBUG
@@ -32,10 +36,32 @@ void printArr(const char* arrName, float *arr, size_t size, int procId) {
 }
 
 void randomSample(float *data, size_t dataSize, float *sample, size_t sampleSize) {
-  for (size_t i=0; i<sampleSize; i++) {
-    sample[i] = data[rand()%dataSize];
+  int i;
+  
+  srand(time(NULL));
+
+  for (i=0; i< sampleSize; i++) {
+    sample[i] = data[i];    
+  }
+  for (; i < dataSize; ++i) {
+    int j = rand() % (i + 1);
+    if (j < sampleSize) {
+      sample[j] = data[i];
+    }
   }
 }
+
+int compare( const void* n1, const void *n2) {
+  return (*(int*)n1 - *(int*)n2);
+
+}
+
+typedef struct
+{
+  float* array;
+  int index;
+} Bucket;
+
 
 void parallelSort(float *data, float *&sortedData, int procs, int procId, size_t dataSize, size_t &localSize) {
   // Implement parallel sort algorithm as described in assignment 3
@@ -57,20 +83,117 @@ void parallelSort(float *data, float *&sortedData, int procs, int procId, size_t
   // Step 3: Redistributing Elements
   // Step 5: Final Local Sort
   // ***********************************************************************
+  
+  double start, end;  
+  // Step 1
+  int s = 12 * log(dataSize);
+  
+  float *pivots = (float*)malloc(sizeof(float) * (procs - 1));
 
+  float *pivotGlobal;
+  start = CycleTimer::currentSeconds();
+  if (procId == 0) {
+    pivotGlobal = (float*)malloc(sizeof(float) * s * procs);
+    
+    randomSample(data, localSize, pivotGlobal, s);
+    
+    MPI_Gather(pivotGlobal, s, MPI_FLOAT, pivotGlobal, s, MPI_FLOAT, 0, MPI_COMM_WORLD);
+    sort(pivotGlobal, pivotGlobal + s * procs);
 
+    pivots[0] = 0;
+    for (int k = 1; k < procs; k++) {
+      pivots[k - 1] = pivotGlobal[k * s];
+    }
 
+    MPI_Bcast(pivots, procs - 1, MPI_FLOAT, 0, MPI_COMM_WORLD);
 
+    
+  } else {
+      float *pivotProc = (float*)malloc(sizeof(float) * s);
+      
+      randomSample(data, localSize, pivotProc, s);
+      //MPI_Send(pivotProc, s, MPI_FLOAT, 0, procId, MPI_COMM_WORLD);
+      MPI_Gather(pivotProc, s, MPI_FLOAT, pivotGlobal, s, MPI_FLOAT, 0, MPI_COMM_WORLD);
+      MPI_Bcast(pivots, procs - 1, MPI_FLOAT, 0, MPI_COMM_WORLD);
+  }
+  end = CycleTimer::currentSeconds();
+  printf("sampling takes %f ms\n", (end - start) * 1000);
+  start = CycleTimer::currentSeconds();
+  
+  // Step 2
 
+ 
+  vector<vector<float> > buckets(procs);
+ 
+  int* counts = (int*)malloc(sizeof(int) * procs);
+  int* disp   = (int*)malloc(sizeof(int) * procs);
+ 
+  memset(counts, 0, sizeof(int) * procs);
 
+  start = CycleTimer::currentSeconds();
+  for (int i = 0; i < localSize; i++) {
+    int bid = lower_bound(pivots, pivots + procs - 1, data[i]) - pivots;
+   
+    buckets[bid].push_back(data[i]);
+    counts[bid]++;
+  }
+  end = CycleTimer::currentSeconds();
+  printf("Finding buckets took %f\n", end - start); 
+  
+  int *recCounts = (int*)malloc(sizeof(int) * procs);
+  //int *counts = (int*)malloc(sizeof(int) * procs);
+  
+  int *displacement = (int*)malloc(sizeof(int) * procs);
+  
+  int d = 0;
+  for (int x = 0; x < procs; x++) {
+    //counts[x] = buckets[x].size();
+    displacement[x] = d;
+    d += counts[x];
+  }
+  
 
+  MPI_Alltoall(counts, 1, MPI_INT, recCounts, 1, MPI_INT, MPI_COMM_WORLD);
+  int tmp = 0;
 
-  // Output:
-  //  sortedData[]: output arrays of sorted data, initially unallocated
-  //                please update the size of sortedData[] to localSize!
-  //  localSize: size of data[] array on *this* process (as input)
-  //             size of sortedData[] array on *this* process (as output)
+  for (int i = 0; i < procs; ++i) {
+    disp[i] = tmp;
+    tmp += recCounts[i];
+  }
+
+  float* bucketsArray = (float*)malloc(sizeof(float) * localSize);
+  int index = 0;
+  for (int i = 0; i < procs; ++i) {
+    for (int j = 0; j < counts[i]; ++j) {
+      bucketsArray[index] = buckets[i][j];
+      index++;
+    }
+  }
+  end = CycleTimer::currentSeconds();
+  printf("step 2 takes %f ms\n", (end - start) * 1000);
+  
+  start = CycleTimer::currentSeconds();
+  
+  //////////////////////////// Step 3 ////////////////////////////////////////////
   localSize = 0;
-  return;
+  for (int i = 0; i < procs; i++) {
+    localSize += recCounts[i];
+  }
+  sortedData = (float*)malloc(sizeof(float) * localSize);
+  
+  MPI_Alltoallv(bucketsArray, counts, displacement, MPI_FLOAT, sortedData, recCounts, disp, MPI_FLOAT, MPI_COMM_WORLD);
+   
+  
+  end = CycleTimer::currentSeconds();
+  printf("process %d exchanging data takes %f ms\n", procId, (end - start) * 1000);
+  
+
+  //////////////////////////////////////// Step 4 ///////////////////////////////////////////////
+  
+  start = CycleTimer::currentSeconds();
+  sort (sortedData, sortedData + localSize);
+  end  = CycleTimer::currentSeconds(); 
+  printf("Sorting takes %f\n", (end - start) * 1000);  
+
 }
 
--- hw/asst3/part2/bfs.cpp
+++ hw/asst3/part2/bfs.cpp
@@ -21,61 +21,57 @@ void vertex_set_init(vertex_set* list, int count) {
     vertex_set_clear(list);
 }
 
-int bottom_up_step(
-        graph* g, 
-        int* distances, int step) {
+int bottom_up_step(graph* g, unsigned char* frontier, unsigned char* new_frontier, int* distances, int step) {
 #ifdef DEBUG
     printf("bottom up\n");
 #endif
     int count = 0;
-    #pragma omp parallel for schedule(dynamic, 500) reduction(+:count)
-    for (int node = 0; node < g->num_nodes; node++) {
-        int private_count = 0;
+    #pragma omp parallel for schedule(dynamic, 400) reduction(+:count)
+    for (int node = 0; node < g->num_nodes; ++node) {  
         // already find distances
-        if (distances[node] != NOT_VISITED_MARKER) {
-            continue;
-        }
-
-        int start_edge = g->incoming_starts[node];
-        int end_edge = (node == g->num_nodes - 1) ? g->num_edges: g->outgoing_starts[node + 1];
-        for (int neighbor = start_edge; neighbor < end_edge; neighbor++) {
+        if (distances[node] == NOT_VISITED_MARKER) {
+         
+          int private_count = 0;
+          int start_edge = g->incoming_starts[node];
+          int end_edge = (node == g->num_nodes - 1) ? g->num_edges: g->outgoing_starts[node + 1];
+           
+          for (int neighbor = start_edge; neighbor < end_edge; neighbor++) {
             int incoming = g->incoming_edges[neighbor];
+            int index = incoming / 8;
+            int offset = incoming % 8;
             // nodes just find in last step
-            if (distances[incoming] == step) {
+            if (frontier[index] & (1 << offset)) { 
                 distances[node] = step + 1;
-                ++count;
-                //if (__sync_bool_compare_and_swap(&distances[node], NOT_VISITED_MARKER, step + 1)) {
-                    //++count;
-                //}
-                //if (__sync_bool_compare_and_swap(&distances[node], NOT_VISITED_MARKER, step + 1)) {
-                    //int index = __sync_fetch_and_add(&new_frontier->count, 1);
-                    //new_frontier->present[index] = node;
-                //}
+                index = node / 8;
+                offset = node % 8;
+                new_frontier[index] |= (1 << offset);
+                ++private_count;
                 break;
             }
         }
         count += private_count;
+      }
+     
     }
     return count;
 }
 
 void bfs_bottom_up(graph* graph, solution* sol)
 {
-    //vertex_set list1;
-    //vertex_set list2;
-    //vertex_set_init(&list1, graph->num_nodes);
-    //vertex_set_init(&list2, graph->num_nodes);
-
-    //vertex_set* frontier = &list1;
-    //vertex_set* new_frontier = &list2;
-
+    int bitmaplen = graph->num_nodes / 8 + 1;
+    unsigned char* bitfrontier = (unsigned char*)malloc(sizeof(char) * bitmaplen);
+    unsigned char* bitnewfrontier = (unsigned char*)malloc(sizeof(char) * bitmaplen);
+ 
+    #pragma omp parallel for schedule(static)
+    for (int i = 0; i < bitmaplen; i++) {
+      bitfrontier[i] &= 0;
+      bitnewfrontier[i] &= 0;
+    }
     #pragma omp parallel for schedule(static)
     for (int i = 0; i < graph->num_nodes; i++) {
         sol->distances[i] = NOT_VISITED_MARKER;
     }
-
-    // setup frontier with the root node
-    //frontier->present[frontier->count++] = ROOT_NODE_ID;
+    bitfrontier[ROOT_NODE_ID] |= 1;
     sol->distances[ROOT_NODE_ID] = 0;
 
     int step = 0;
@@ -85,9 +81,13 @@ void bfs_bottom_up(graph* graph, solution* sol)
         double start_time = CycleTimer::currentSeconds();
 #endif
 
+        count = bottom_up_step(graph, bitfrontier, bitnewfrontier, sol->distances, step++);
 
-        count = bottom_up_step(graph, sol->distances, step++);
-
+        #pragma omp parallel for schedule(static)
+        for (int i = 0; i < bitmaplen; i++) {
+          bitfrontier[i] = bitnewfrontier[i];
+          bitnewfrontier[i] = 0;
+        }
 #ifdef DEBUG
         double end_time = CycleTimer::currentSeconds();
         printf("%.4f sec\n", end_time - start_time);
@@ -98,38 +98,35 @@ void bfs_bottom_up(graph* graph, solution* sol)
 // Take one step of "top-down" BFS.  For each vertex on the frontier,
 // follow all outgoing edges, and add all neighboring vertices to the
 // new_frontier.
-void top_down_step(
-    graph* g,
-    vertex_set* frontier,
-    vertex_set* new_frontier,
-    int* distances
-    )
-{
-
+int top_down_step(graph* g, unsigned char* frontier, unsigned char* new_frontier, int* distances, int step) {
 #ifdef DEBUG
     printf("top down\n");
 #endif
-    #pragma omp parallel for schedule(dynamic, 500)
-    for (int i=0; i<frontier->count; i++) {
-
-        int node = frontier->present[i];
-
+    int count = 0;
+    #pragma omp parallel for schedule(dynamic, 500) reduction(+: count)
+    for (int node = 0; node < g->num_nodes; ++node) {
+        if (distances[node] != step) {
+            continue;
+        }
+        int private_count = 0;
         int start_edge = g->outgoing_starts[node];
         int end_edge = (node == g->num_nodes-1) ? g->num_edges : g->outgoing_starts[node+1];
 
         // attempt to add all neighbors to the new frontier
         for (int neighbor=start_edge; neighbor<end_edge; neighbor++) {
             int outgoing = g->outgoing_edges[neighbor];
-            if (distances[outgoing] != NOT_VISITED_MARKER) {
-                continue;
-            }
-
-            if (__sync_bool_compare_and_swap(&distances[outgoing], NOT_VISITED_MARKER, distances[node] + 1)) {
-                int index = __sync_fetch_and_add(&new_frontier->count, 1);
-                new_frontier->present[index] = outgoing;
+            if (distances[outgoing] == NOT_VISITED_MARKER) {
+                int index = outgoing / 8;
+                int offset = outgoing % 8;
+                new_frontier[index] |= (1 << offset);
+                //new_frontier->present[outgoing] = true;
+                distances[outgoing] = step + 1;
+                ++private_count;
             }
         }
+        count += private_count;
     }
+    return count;
 }
 
 // Implements top-down BFS.
@@ -138,95 +135,109 @@ void top_down_step(
 // distance to the root is stored in sol.distances.
 void bfs_top_down(graph* graph, solution* sol) {
 
-    vertex_set list1;
-    vertex_set list2;
-    vertex_set_init(&list1, graph->num_nodes);
-    vertex_set_init(&list2, graph->num_nodes);
+    int bitmaplen = graph->num_nodes / 8 + 1;
+    unsigned char* bitfrontier = (unsigned char*)malloc(sizeof(char) * bitmaplen);
+    unsigned char* bitnewfrontier = (unsigned char*)malloc(sizeof(char) * bitmaplen);
 
-    vertex_set* frontier = &list1;
-    vertex_set* new_frontier = &list2;
-
-    // initialize all nodes to NOT_VISITED
     #pragma omp parallel for schedule(static)
-    for (int i=0; i<graph->num_nodes; i++)
-        sol->distances[i] = NOT_VISITED_MARKER;
+    for (int i = 0; i < bitmaplen; i++) {
+      bitfrontier[i] &= 0;
+      bitnewfrontier[i] &= 0;
+    }
 
+
+    // initialize all nodes to NOT_VISITED  
+    #pragma omp parallel for schedule(static) 
+    for (int i=0; i<graph->num_nodes; i++) {
+        sol->distances[i] = NOT_VISITED_MARKER;
+    } 
     // setup frontier with the root node
-    frontier->present[frontier->count++] = ROOT_NODE_ID;
     sol->distances[ROOT_NODE_ID] = 0;
 
-    while (frontier->count != 0) {
+    int step = 0;
+    int count = 1;
+    while (count != 0) {
 
 #ifdef DEBUG
         double start_time = CycleTimer::currentSeconds();
 #endif
 
-        vertex_set_clear(new_frontier);
-
-        top_down_step(graph, frontier, new_frontier, sol->distances);
+        count = top_down_step(graph, bitfrontier, bitnewfrontier, sol->distances, step++);
 
 #ifdef DEBUG
         double end_time = CycleTimer::currentSeconds();
-        printf("frontier=%-10d %.4f sec\n", frontier->count, end_time - start_time);
+        printf("%.4f sec\n", end_time - start_time);
 #endif
-
-        // swap pointers
-        vertex_set* tmp = frontier;
-        frontier = new_frontier;
-        new_frontier = tmp;
+        #pragma omp parallel for schedule(static)
+        for (int i = 0; i < bitmaplen; i++) {
+          bitfrontier[i] = bitnewfrontier[i];
+          bitnewfrontier[i] = 0;
+        }
+        
     }
 }
 
 void bfs_hybrid(graph* graph, solution* sol) {
 
-    vertex_set list1;
-    vertex_set list2;
-    vertex_set_init(&list1, graph->num_nodes);
-    vertex_set_init(&list2, graph->num_nodes);
+    int switchNum = (int)(0.2 * graph->num_nodes);
+    int t1 = (int)(0.48 * graph->num_nodes);
+    int t2 = (int)(0.2 * graph->num_nodes);
 
-    vertex_set* frontier = &list1;
-    vertex_set* new_frontier = &list2;
+    int bitmaplen = graph->num_nodes / 8 + 1;
+    unsigned char* bitfrontier = (unsigned char*)malloc(sizeof(char) * bitmaplen);
+    unsigned char* bitnewfrontier = (unsigned char*)malloc(sizeof(char) * bitmaplen);
 
-    // initialize all nodes to NOT_VISITED
     #pragma omp parallel for schedule(static)
-    for (int i=0; i<graph->num_nodes; i++)
+    for (int i = 0; i < bitmaplen; i++) {
+      bitfrontier[i] &= 0;
+      bitnewfrontier[i] &= 0;
+    }
+
+    // initialize all nodes to NOT_VISITED 
+    #pragma omp parallel for schedule(static) 
+    for (int i=0; i<graph->num_nodes; i++) {
         sol->distances[i] = NOT_VISITED_MARKER;
-
+    } 
     // setup frontier with the root node
-    frontier->present[frontier->count++] = ROOT_NODE_ID;
     sol->distances[ROOT_NODE_ID] = 0;
-
-    int switchNum = (int)(0.07 * graph->num_nodes);
-#ifdef DEBUG
-    printf("switch num: %d\n", switchNum);
-#endif
-
+    
     int step = 0;
-    while (frontier->count != 0) {
+    int count = 1;
+    int prev_count = 0;
 
+    while (count != 0) {
+
+        if (count >  switchNum) {
+          break;
+        }
+        
 #ifdef DEBUG
         double start_time = CycleTimer::currentSeconds();
 #endif
-        vertex_set_clear(new_frontier);
 
-        if (frontier->count > switchNum) {
-            break;
-        }
-        ++step;
-        top_down_step(graph, frontier, new_frontier, sol->distances);
+
+        count = top_down_step(graph, bitfrontier, bitnewfrontier, sol->distances, step++);
 
 #ifdef DEBUG
         double end_time = CycleTimer::currentSeconds();
-        printf("frontier=%-10d %.4f sec\n", frontier->count, end_time - start_time);
+        printf("%.4f sec\n", end_time - start_time);
 #endif
-        // swap pointers
-        vertex_set* tmp = frontier;
-        frontier = new_frontier;
-        new_frontier = tmp;
-    }
-    int count = 1;
-    while (count != 0) {
-        count = bottom_up_step(graph, sol->distances, step);
-        ++step;
+
+        #pragma omp parallel for schedule(static)
+        for (int i = 0; i < bitmaplen; i++) {
+          bitfrontier[i] = bitnewfrontier[i];
+          bitnewfrontier[i] = 0;
+        }
+      } 
+      count = 1;
+      while (count != 0) {
+       
+        count = bottom_up_step(graph, bitfrontier, bitnewfrontier,  sol->distances, step++);
+
+        #pragma omp parallel for schedule(static)
+        for (int i = 0; i < bitmaplen; i++) {
+          bitfrontier[i] = bitnewfrontier[i];
+          bitnewfrontier[i] = 0;
+        }
     }
 }
