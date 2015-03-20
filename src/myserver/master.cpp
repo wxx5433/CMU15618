eb76928a6e423cf160d21d92fec2bdeeeb319042
wxx5433 <wxx5433@gmail.com>
2015-03-20 17:18:23 -0400

    finish worker

--- src/myserver/master.cpp
+++ src/myserver/master.cpp
@@ -1,10 +1,13 @@
 #include <glog/logging.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <map>
+#include <vector>
 
 #include "server/messages.h"
 #include "server/master.h"
 
+using namespace std;
 
 static struct Master_state {
 
@@ -18,8 +21,10 @@ static struct Master_state {
   int num_pending_client_requests;
   int next_tag;
 
-  Worker_handle my_worker;
-  Client_handle waiting_client;
+  // key: worker tag, value: worker_handle
+  map<int, Worker_handle> my_worker;
+  // key: request tag, value: client_handle
+  map<int, Client_handle> waiting_client;
 
 } mstate;
 
@@ -47,6 +52,13 @@ void master_node_init(int max_workers, int& tick_period) {
   req.set_arg("name", "my worker 0");
   request_new_worker_node(req);
 
+  //for (int i = 0; i < max_workers; ++i) {
+      //int tag = mstate.next_tag++;
+      //Request_msg req(tag);
+      //req.set_arg("name", "my worker " + tag);
+      //request_new_worker_node(req);
+  //}
+
 }
 
 void handle_new_worker_online(Worker_handle worker_handle, int tag) {
@@ -55,7 +67,8 @@ void handle_new_worker_online(Worker_handle worker_handle, int tag) {
   // corresponds to.  Since the starter code only sends off one new
   // worker request, we don't use it here.
 
-  mstate.my_worker = worker_handle;
+  //mstate.my_worker = worker_handle;
+  mstate.my_worker[tag] = worker_handle;
 
   // Now that a worker is booted, let the system know the server is
   // ready to begin handling client requests.  The test harness will
@@ -73,9 +86,9 @@ void handle_worker_response(Worker_handle worker_handle, const Response_msg& res
 
   DLOG(INFO) << "Master received a response from a worker: [" << resp.get_tag() << ":" << resp.get_response() << "]" << std::endl;
 
-  send_client_response(mstate.waiting_client, resp);
-
-  mstate.num_pending_client_requests = 0;
+  send_client_response(worker_handle, resp);
+  //mstate.num_pending_client_requests = 0;
+  mstate.num_pending_client_requests--;
 }
 
 void handle_client_request(Client_handle client_handle, const Request_msg& client_req) {
@@ -92,28 +105,20 @@ void handle_client_request(Client_handle client_handle, const Request_msg& clien
     return;
   }
 
-  // The provided starter code cannot handle multiple pending client
-  // requests.  The server returns an error message, and the checker
-  // will mark the response as "incorrect"
-  if (mstate.num_pending_client_requests > 0) {
-    Response_msg resp(0);
-    resp.set_response("Oh no! This server cannot handle multiple outstanding requests!");
-    send_client_response(client_handle, resp);
-    return;
-  }
-
   // Save off the handle to the client that is expecting a response.
   // The master needs to do this it can response to this client later
   // when 'handle_worker_response' is called.
-  mstate.waiting_client = client_handle;
+  //mstate.waiting_client = client_handle;
   mstate.num_pending_client_requests++;
 
   // Fire off the request to the worker.  Eventually the worker will
   // respond, and your 'handle_worker_response' event handler will be
   // called to forward the worker's response back to the server.
+  // TODO need to consider load balancing
   int tag = mstate.next_tag++;
+  mstate.waiting_client[tag] = client_handle;
   Request_msg worker_req(tag, client_req);
-  send_request_to_worker(mstate.my_worker, worker_req);
+  send_request_to_worker(mstate.my_worker[0], worker_req);
 
   // We're done!  This event handler now returns, and the master
   // process calls another one of your handlers when action is
@@ -122,6 +127,7 @@ void handle_client_request(Client_handle client_handle, const Request_msg& clien
 }
 
 
+// TODO should add/kill workers according to pending requests number
 void handle_tick() {
 
   // TODO: you may wish to take action here.  This method is called at
