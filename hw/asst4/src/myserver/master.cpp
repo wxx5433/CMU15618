c7a1029dfaf56ca7afbdf449aaf5235b88fee50c
wxx5433 <wxx5433@gmail.com>
2015-03-21 16:44:44 -0400

    add cache

--- hw/asst4/src/myserver/master.cpp
+++ hw/asst4/src/myserver/master.cpp
@@ -40,18 +40,26 @@ static struct Master_state {
   // workers that are processing reqeusts
   list<Worker_handle> working_workers;
   // workers that has no processing power
-  queue<Worker_handle> busy_workers;
+  list<Worker_handle> busy_workers;
 
   // all requests that need to be processed
   queue<Request_msg> request_queue;
 
+  // key: worker handle, value: worker infomation
   map<Worker_handle, Info> worker_info;
   // key: request tag, value: client handle
   map<int, Client_handle> waiting_client;
 
+  // key: request tag, value: request string 
+  map<int, string> request_map;
+  // request cache, key: request string, value: response msg
+  map<string, Response_msg> request_cache;
+
 } mstate;
 
 
+bool check_cache(Client_handle, const Request_msg&);
+Client_handle get_client_handle(int tag);
 
 void master_node_init(int max_workers, int& tick_period) {
 
@@ -72,10 +80,12 @@ void master_node_init(int max_workers, int& tick_period) {
   mstate.server_ready = false;
 
   // fire off a request for a new worker
-  int tag = mstate.next_tag++;
-  Request_msg req(tag);
-  req.set_arg("name", "my worker " + tag);
-  request_new_worker_node(req);
+  for (int i = 0; i < max_workers; ++i) {
+    int tag = mstate.next_tag++;
+    Request_msg req(tag);
+    req.set_arg("name", "my worker " + tag);
+    request_new_worker_node(req);
+  }
 }
 
 /* 
@@ -84,7 +94,6 @@ void master_node_init(int max_workers, int& tick_period) {
  */
 void handle_new_worker_online(Worker_handle worker_handle, int tag) {
   // add the new worker to free workers queue
-  mstate.free_workers.push(worker_handle);
   Info info;
   info.state = FREE;
   info.processing_request_num = 0;
@@ -119,11 +128,37 @@ void handle_worker_response(Worker_handle worker_handle, const Response_msg& res
     DLOG(INFO) << it->first << " => " << it->second << endl;
   }
 #endif
-  map<int,Client_handle>::iterator client_it = mstate.waiting_client.find(resp_tag);
-  if (client_it != mstate.waiting_client.end()) {
-    send_client_response(client_it->second, resp);
+  Client_handle client_handle = get_client_handle(resp_tag);
+  send_client_response(client_handle, resp);
+  mstate.num_pending_client_requests--;
+
+  // add response message to cache
+  map<int, string>::iterator request_it = mstate.request_map.find(resp_tag);
+  if (request_it != mstate.request_map.end()) {
+    string request_string = request_it->second;
+    mstate.request_cache[request_string] = resp;
   } else {
-    DLOG(INFO) << "Cannot find client" << endl;
+    DLOG(INFO) << "Cannot find tag" << endl;
+  }
+  mstate.request_map.erase(resp_tag);
+
+  // if request queue is not empty, fetch one more
+  // TODO while
+  while (!mstate.request_queue.empty()) {
+    Request_msg client_req = mstate.request_queue.front();
+    mstate.request_queue.pop();
+    Client_handle client_handle = get_client_handle(client_req.get_tag());
+    // TODO
+    if (check_cache(client_handle, client_req)) {
+      continue;
+    }
+    map<Worker_handle,Info>::iterator info_it 
+        = mstate.worker_info.find(worker_handle);
+    Info info = info_it->second;
+    //info.processing_request_num++;
+    DLOG(INFO) << "send a request to worker " << info.tag << ", request num: " << info.processing_request_num << endl;
+    send_request_to_worker(worker_handle, client_req);
+    return;
   }
 
   map<Worker_handle,Info>::iterator info_it = mstate.worker_info.find(worker_handle);
@@ -135,29 +170,26 @@ void handle_worker_response(Worker_handle worker_handle, const Response_msg& res
       mstate.working_workers.remove(worker_handle);
       // add to free workers list
       mstate.free_workers.push(worker_handle);
+    } else if (info.processing_request_num == WORKER_NUM - 1) {
+      info.state = WORKING;
+      mstate.busy_workers.remove(worker_handle);
+      mstate.working_workers.insert(mstate.working_workers.begin(), worker_handle);
     }
     mstate.worker_info[worker_handle] = info;
   } else {
     DLOG(INFO) << "Cannot find worker info" << endl;
   }
 
-  mstate.num_pending_client_requests--;
-
-  // if request queue is not empty, fetch one more
-  if (!mstate.request_queue.empty()) {
-    Request_msg worker_req = mstate.request_queue.front();
-    mstate.request_queue.pop();
-    send_request_to_worker(worker_handle, worker_req);
-    map<Worker_handle,Info>::iterator info_it 
-        = mstate.worker_info.find(worker_handle);
-    Info info = info_it->second;
-    DLOG(INFO) << "send a request to worker " << info.tag << ", request num: " << info.processing_request_num << endl;
-  }
 }
 
 void handle_client_request(Client_handle client_handle, const Request_msg& client_req) {
 
   DLOG(INFO) << "Received request: " << client_req.get_request_string() << std::endl;
+
+  // check cache
+  if (check_cache(client_handle, client_req)) {
+    return;
+  }
 
   // You can assume that traces end with this special message.  It
   // exists because it might be useful for debugging to dump
@@ -174,6 +206,7 @@ void handle_client_request(Client_handle client_handle, const Request_msg& clien
   // when 'handle_worker_response' is called.
   int tag = mstate.next_tag++;
   mstate.waiting_client[tag] = client_handle;
+  mstate.request_map[tag] = client_req.get_request_string();
 #ifdef DEBUG
   DLOG(INFO) << "add request" << endl;
   for (std::map<int,Client_handle>::iterator it=mstate.waiting_client.begin(); 
@@ -205,8 +238,12 @@ void handle_client_request(Client_handle client_handle, const Request_msg& clien
     if (info.processing_request_num == WORKER_NUM) {
       info.state = BUSY;
       // add to busy workers
-      mstate.working_workers.pop_front();
-      mstate.busy_workers.push(worker);
+      mstate.working_workers.remove(worker);
+      mstate.busy_workers.push_back(worker);
+      DLOG(INFO) << "Add worker " << info.tag << " to busy queue" << endl;
+      DLOG(INFO) << "free workers num: " << mstate.free_workers.size() << endl;
+      DLOG(INFO) << "working workers num: " << mstate.working_workers.size() << endl;
+      DLOG(INFO) << "full workers num: " << mstate.busy_workers.size() << endl;
     }
     mstate.worker_info[worker] = info;
     // send request
@@ -231,6 +268,10 @@ void handle_client_request(Client_handle client_handle, const Request_msg& clien
     mstate.worker_info[worker] = info;
     // add to working workers list
     mstate.working_workers.push_back(worker);
+    DLOG(INFO) << "Add worker " << info.tag << " to working queue" << endl;
+    DLOG(INFO) << "free workers num: " << mstate.free_workers.size() << endl;
+    DLOG(INFO) << "working workers num: " << mstate.working_workers.size() << endl;
+    DLOG(INFO) << "full workers num: " << mstate.busy_workers.size() << endl;
     // send request
     send_request_to_worker(worker, worker_req);
 
@@ -250,6 +291,7 @@ void handle_tick() {
   // TODO: you may wish to take action here.  This method is called at
   // fixed time intervals, according to how you set 'tick_period' in
   // 'master_node_init'.
+  /*
   if (mstate.worker_num < mstate.max_num_workers) {
     int tag = mstate.next_tag++;
     Request_msg req(tag);
@@ -257,4 +299,28 @@ void handle_tick() {
     request_new_worker_node(req);
     mstate.worker_num++;
   }
+  */
+}
+
+bool check_cache(Client_handle client_handle, const Request_msg& client_req) {
+  map<string, Response_msg>::iterator request_it = mstate.request_cache.find(client_req.get_request_string());
+  if (request_it != mstate.request_cache.end()) {
+    Response_msg resp = request_it->second;
+    // reset tag number
+    resp.set_tag(mstate.next_tag++);
+    send_client_response(client_handle, resp);
+    DLOG(INFO) << "Cache hit: " << client_req.get_request_string() << std::endl;
+    return true;
+  }
+  return false; 
+}
+
+Client_handle get_client_handle(int tag) {
+  map<int,Client_handle>::iterator client_it = mstate.waiting_client.find(tag);
+#ifdef DEBUG
+  if (client_it == mstate.waiting_client.end()) {
+    DLOG(INFO) << "Cannot find client" << endl;
+  }
+#endif
+  return client_it->second;
 }
